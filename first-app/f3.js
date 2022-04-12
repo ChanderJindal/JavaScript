fb1746126ec7c9d19e716204591aa5058749a901
cj-kid <chanderjindal2@gmail.com>
2022-04-12 21:55:58 +0530

    export

---
+++ first-app/f3.js
@@ -0,0 +1,13 @@
+var url = "https://gogoanime.gg/category/detective-conan";
+
+function log(message){
+    // Send HTTPS Req.
+    console.log(message);
+}
+
+module.exports.log = log;
+//module.exports.OutSideName = Name Here;
+// with this module is exported
+
+//module.exports = log 
+// here only the funcion is exported
---
+++ first-app/f4.js
@@ -0,0 +1,11 @@
+
+var logger = require('./f3.js') // path here '.' is for start from this destination.
+
+console.log(logger)
+
+logger.log("asdfghjk")
+
+logger = 2
+// overrided logger type and value
+console.log(logger)
+
